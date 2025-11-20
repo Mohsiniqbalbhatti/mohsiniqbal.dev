faa1c730ea99f643e34aa5a247540666f3773c7a
Muhammad Mohsin Iqbal <147073902+Mohsiniqbalbhatti@users.noreply.github.com>
2025-11-20 21:13:09 +0500

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -8,11 +8,11 @@ export const BIO_LONG = `I am a passionate Full Stack Engineer specializing in R
 export const SKILLS: Skill[] = [
   { name: 'React / Next.js', level: 95, category: 'Frontend' },
   { name: 'TypeScript', level: 90, category: 'Frontend' },
-  { name: 'MongoDB/Postgres', level: 75, category: 'DataBase' },
+  { name: 'MongoDB / Postgres', level: 75, category: 'DataBase' },
   { name: 'Node.js', level: 80, category: 'Backend' },
   { name: 'Three.js / FramerMotion', level: 85, category: 'Frontend' },
   { name: 'GoLang', level: 88, category: 'Backend' },
-  { name: 'Docker/AWS', level: 70, category: 'Tools' },
+  { name: 'Docker / AWS', level: 70, category: 'Tools' },
 ];
 
 export const PROJECTS: Project[] = [
